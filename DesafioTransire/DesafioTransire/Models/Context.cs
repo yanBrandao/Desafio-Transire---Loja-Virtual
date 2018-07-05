c36d385e77376505ae70d67e37969256bff8f052
yanbranda-PC <ybrandao.d@gmail.com>
2018-07-05 00:33:49 -0400

    [CSS] Added CSS file Lumen for more clean website. [Models] Added Model for produtcs. [Entity] Used Entity framework to work with Product.

---
+++ DesafioTransire/DesafioTransire/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DesafioTransire.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
